5a01abb62cb0fe11a9b813dbe604bf9956345c29
Md Aamir <asadahmadkh344@gmail.com>
2021-11-08 19:17:51 +0530

    new program

---
+++ src/com/Javapractice/Main.java
@@ -0,0 +1,14 @@
+package com.Javapractice;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
